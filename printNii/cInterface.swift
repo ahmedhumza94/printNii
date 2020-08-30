6930a474757fc857877077505e039a00697ff020
Humza Ahmed <ahmedhumza94@gmail.com>
2020-08-30 16:08:58 -0500

    Separate c and nii interfaces

---
+++ printNii/cInterface.swift
@@ -0,0 +1,9 @@
+//
+//  cInterface.swift
+//  printNii
+//
+//  Created by Humza Ahmed on 8/30/20.
+//  Copyright © 2020 Humza Ahmed. All rights reserved.
+//
+
+import Foundation
---
+++ printNii/niiInterface.swift
@@ -0,0 +1,9 @@
+//
+//  niiInterface.swift
+//  printNii
+//
+//  Created by Humza Ahmed on 8/30/20.
+//  Copyright © 2020 Humza Ahmed. All rights reserved.
+//
+
+import Foundation
