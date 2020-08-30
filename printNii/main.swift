1cb99ec140a5078555775cfcb54077abcc3c19b2
Humza Ahmed <ahmedhumza94@gmail.com>
2020-08-30 18:04:30 -0500

    Print in order of nifti 1 spec

--- printNii/main.swift
+++ printNii/main.swift
@@ -14,9 +14,31 @@ struct printNii: ParsableCommand {
     
     mutating func run() throws {
         let niiHdr = NiiHdr(file)
+        let fieldOrder: [String] = [
+            //key substruct
+            "sizeof_hdr","dataType","dbName",
+            "extents","sessionError","regular",
+            "dimInfo","dim",
+            //dim substruct
+            "intent_p1","intent_p2","intent_p3",
+            "intent_code","datatype","bitpix",
+            "slice_start","pixdim","vox_offset",
+            "scl_slope","scl_inter","slice_end",
+            "slice_code","xyzt_units","cal_max",
+            "cal_min","slice_duration","toffset",
+            //data history substruct
+            "glmax","glmin","descrip",
+            "aux_file","qform_code","sform_code",
+            "quatern_b","quatern_c","quatern_d",
+            "qoffset_x","qoffset_y","qoffset_z",
+            "srow_x","srow_y","srow_z",
+            "intent_name","magic"
+            
+        ]
+        
         var text: String
-        for (field, value) in niiHdr.hdr {
-            text = field + ": " + printCStoreArray(value)
+        for currField in fieldOrder {
+            text = currField + ": " + printCStoreArray(niiHdr.hdr[currField]!)
             print(text)
         }
     }
